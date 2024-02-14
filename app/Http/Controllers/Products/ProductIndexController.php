ff347d37d4ec76936784f34cdc5aca4b0bcbac72
Cristian Vasquez <cvo6372@gmail.com>
2024-02-14 17:32:38 -0500

    relations in models

--- app/Http/Controllers/Products/ProductIndexController.php
+++ app/Http/Controllers/Products/ProductIndexController.php
@@ -12,6 +12,8 @@ final class ProductIndexController
 
     public function __invoke(): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
-        return response($this->product->where('status', 1)->get()->toArray(), 200);
+        return response($this->product->with(['user' => function ($query) {
+            $query->select('id', 'name', 'email');
+        }])->where('status', 1)->get()->toArray(), 200);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -12,4 +13,9 @@ class Product extends Model
     protected $fillable = [
         'name', 'price', 'status', 'user_creator_id', 'user_last_update', 'created_at', 'updated_at'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_creator_id', 'id');
+    }
 }
